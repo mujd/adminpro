4c919deadb72c5ca75117c3d2f09883bf2b99945
Mujd <mujd19@gmail.com>
2018-10-19 22:43:47 -0300

    Input, output y viewchild

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 // Modulos
 import { SharedModule } from '../shared/shared.module';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
 
 import { PagesComponent } from './pages.component';
 
@@ -12,12 +16,18 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 // Rutas
 import { PAGES_ROUTES } from './pages.routes';
 
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficoDonaComponent } from '../components/grafico-dona/grafico-dona.component';
+
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficoDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -26,7 +36,9 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ],
     providers: [],
 })
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-progress',
-  templateUrl: './progress.component.html',
+  selector: "app-progress",
+  templateUrl: "./progress.component.html",
   styles: []
 })
 export class ProgressComponent implements OnInit {
+  progreso1: number = 20;
+  progreso2: number = 30;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
+  /* actualizar(event: number) {
+    console.log("Evento", event);
+  } */
 }
